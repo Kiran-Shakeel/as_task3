8047789c6316a214ad43d739efb7ecfde1b75abb
Kiran-Shakeel <kiranshakeel02@gmail.com>
2021-08-05 01:48:17 +0500

    done

--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -18,48 +18,70 @@ class _GalleryState extends State<Gallery> {
         length: items.length,
         child: Builder(builder: (BuildContext context) {
           return Scaffold(
-            appBar: HomeBar(),
-            body: TabBarView(
-              children: items.map((TabItems choice) {
-                return Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: GalleryPage(
-                    index: DefaultTabController.of(context)!.index,
+              extendBody: true,
+              appBar: HomeBar(),
+              body: TabBarView(
+                children: items.map((TabItems choice) {
+                  return Padding(
+                    padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+                    child: GalleryPage(
+                      index: DefaultTabController.of(context)!.index,
+                    ),
+                  );
+                }).toList(),
+              ),
+              bottomNavigationBar: Padding(
+                padding: EdgeInsets.only(bottom: 30, left: 32, right: 32),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Container(
+                    height: 70,
+                    color: Colors.white,
+                    width: MediaQuery.of(context).size.width,
+                    child: Expanded(
+                      child: BottomNavigationBar(
+                        type: BottomNavigationBarType.fixed,
+                        items: <BottomNavigationBarItem>[
+                          BottomNavigationBarItem(
+                              icon: Icon(
+                               
+                                Icons.camera_alt_outlined,
+                                color: Colors.teal,
+                              ),
+                              label: ''),
+                          BottomNavigationBarItem(
+                              icon: Icon(
+                                Icons.photo_outlined,
+                                color: Colors.pink,
+                              ),
+                              label: ''),
+                          BottomNavigationBarItem(
+                              icon: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(30),
+                                      color: Colors.blue),
+                                  height: 40,
+                                  width: 40,
+                                  child: Icon(Icons.close)),
+                              label: ''),
+                          BottomNavigationBarItem(
+                              icon: Icon(
+                                CupertinoIcons.compass,
+                                color: Colors.yellow,
+                              ),
+                              label: ''),
+                          BottomNavigationBarItem(
+                              icon: Icon(
+                                Icons.note_outlined,
+                                color: Colors.purple,
+                              ),
+                              label: ''),
+                        ],
+                      ),
+                    ),
                   ),
-                );
-              }).toList(),
-            ),
-            bottomNavigationBar: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.camera_alt_outlined,
-                      color: Colors.teal,
-                    ),
-                    label: ''),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.photo_outlined,
-                      color: Colors.pink,
-                    ),
-                    label: ''),
-                BottomNavigationBarItem(icon: Icon(Icons.close), label: ''),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      CupertinoIcons.compass,
-                      color: Colors.yellow,
-                    ),
-                    label: ''),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.note_outlined,
-                      color: Colors.purple,
-                    ),
-                    label: ''),
-              ],
-            ),
-          );
+                ),
+              ));
         }),
       ),
     );
@@ -80,7 +102,7 @@ class _GalleryPageState extends State<GalleryPage> {
     return Column(children: [
       Expanded(
         child: GridView.count(
-            crossAxisCount: 4,
+            crossAxisCount: 2,
             mainAxisSpacing: 10,
             crossAxisSpacing: 10,
             children: List.generate(21, (index) {
